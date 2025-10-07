34c0dbef6820387e529557c76c500c87e2832d5b
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2025-10-07 16:01:53 +0200

    Initial commit of shopping list application

--- js/index.js
+++ js/index.js
@@ -1,2 +1,41 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+function plus(a,b){
+    return a+b;
+}
+
+let result = plus(3,4);
+console.log(result);
+
+function findnode(id){
+    let node = document.getElementById(id);
+    node.style.color = "red";
+    node.style.backgroundColor = "black";
+    node.style.fontSize = "30px";
+    node.style.fontWeight = "bold";
+
+    //let nynode = document.createElement(type);
+    //nynode.innerHTML = "I am a new node";
+    //nynode.style.color = "blue";
+    //document.body.appendChild(nynode);
+
+    return node;
+}
+
+//let newnode = findnode("2","li");
+//console.log("New node is:");
+//console.log(newnode);
+
+function CreateLiElement(itemId, className, item){
+    let li = document.createElement("li");
+    li.setAttribute("id",itemId);
+    li.setAttribute("class",className);
+    let textNode = document.createTextNode(item);
+    li.appendChild(textNode);
+    return li;
+}
+
+let newli = CreateLiElement("4","healthy","Milk");
+console.log("New li element:");
+console.log(newli);
